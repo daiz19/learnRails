15805895ec8171b9da04463358a156fa07b51118
hani <daiz19@gmail.com>
2020-04-16 23:12:55 +0800

    added CRUD for work

--- Movielog/app/controllers/works_controller.rb
+++ Movielog/app/controllers/works_controller.rb
@@ -2,4 +2,40 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+  def show
+    @work = Work.find(params[:id])
+  end
+  def new
+    @work = Work.new
+  end
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to works_path
+    else
+      render new
+    end
+  end
+  def edit
+    @work = Work.find(params[:id])
+  end
+  def update
+    @work = Work.find(params[:id])
+    if @work.update(work_params)
+      redirect_to work_path(@work)
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @work = Work.find(params[:id])
+    @work.destroy
+    redirect_to works_path
+  end
+
+  private
+
+  def work_params
+    params[:work].permit!
+  end
 end
